45211ab626596d8fbd25ac291a5445b773478fbc
Gioele Bernardi <gioele.bernardi@immobiliare.it>
2024-07-01 18:29:20 +0200

    feat: init content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://matrimoniomarcoekatia.pages.github.io",
+  site: "https://gioeleber.github.io",
   base: "matrimoniomarcoekatia",
+  integrations: [tailwind()]
 });
