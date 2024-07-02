d221866ee3f1cb391435883b3799c4cc32559041
Gioele Bernardi <gioele.bernardi@immobiliare.it>
2024-07-02 11:50:27 +0200

    feat: add preliminary style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: [
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      serif: [
+        "ui-serif",
+        "Georgia, Cambria",
+        "Times New Roman",
+        "Times",
+        "serif",
+      ],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
